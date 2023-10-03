2d2d6aa0b87735b637a1fc7508327c25b15e3b1b
fabischor <103068339+fabischor@users.noreply.github.com>
2023-10-03 08:32:15 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(500,500);
+    background("lightblue");
+  }
+  
+  function draw () {
+    fill("yellow");
+    circle(250,250,300);
+    fill("darkblue");
+    rect(0,300,500,350);
+    fill("khaki");
+    rect(0,400,500,100);
+  }
